200bc466443315b2e6489c7ab917fe0cf6c0dcda
Fedor Aptekarev <aptekarev@gmail.com>
2020-02-24 21:49:58 +0300

    Add launchers

---
+++ app-run-shell.sh
@@ -0,0 +1,2 @@
+docker-compose up -d
+xhost + 127.0.0.1 && docker exec -it -u ofuser helyx-os-for-mac_cfd_apps_1 /bin/bash
---
+++ app-run.sh
@@ -0,0 +1,2 @@
+docker-compose up -d
+xhost + 127.0.0.1 && docker exec -it cfd_containers_cfd_apps_1 /bin/bash
