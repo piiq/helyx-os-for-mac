3b37430ec73de2065b863252e21e4307c4821a30
Theodore Aptekarev <aptekarev@gmail.com>
2022-07-13 00:37:19 +0200

    Update launch scripts

--- app-run.sh
+++ app-run.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 docker-compose up -d
-xhost + 127.0.0.1 && docker exec -it helyx-os-for-mac_cfd_apps_1 /home/ofuser/Engys/HELYX-OS/v2.4.0/HELYX-OS.sh
+xhost + 127.0.0.1 && docker exec -it helyx-os /home/ofuser/Engys/HELYX-OS/v2.4.0/HELYX-OS.sh
